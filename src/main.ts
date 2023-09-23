8811fd74022a58ecfe70d30198952ce6a737f639
fcanmekikoglu <fcanmekikoglu@gmail.com>
2023-09-23 18:25:49 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // For API versioning. its v1 except auth route
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix("v1", { exclude: ["auth"] });
+
+  // For class validator. Enables payload whitelisting globally.
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // OpenAPI Swagger Section
+  const config = new DocumentBuilder().setTitle("NestJs API").setDescription("Boilerplate").setVersion("1.0").addTag("api").build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  // Serve on 3000
+  await app.listen(3000);
+}
+bootstrap();
