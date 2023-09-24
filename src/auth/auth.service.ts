bfcf3ed99834c827d1694f40f849df52f71d54ed
F. Can Mekikoğlu <fcanmekikoglu@gmail.com>
2023-09-24 20:23:30 +0300

    removed jsdoc

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { BadRequestException, Injectable, Logger } from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
 import { Tokens } from "./types/tokens.type";
 import { SignupDto } from "./dto/signup.dto";
-
 import * as bcrypt from "bcrypt";
 import { JwtService } from "@nestjs/jwt";
 import { UserDocument } from "src/users/schemas/user.schema";
@@ -19,25 +18,7 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly mailService: MailService,
   ) {}
-
-  /**
-   * Handles user signup and token generation.
-   *
-   * @async
-   * @param {SignupDto} signupDto - Data transfer object containing the user's email and password.
-   * @returns {Promise<Tokens>} An object containing the accessToken and refreshToken for the signed-up user.
-   * @throws {BadRequestException} Throws an exception if the email is already in use.
-   *
-   * @example
-   * const signupData = {
-   *   email: "example@example.com",
-   *   password: "Password123+"
-   * };
-   * const tokens = await signup(signupData);
-   * console.log(tokens.accessToken);  // Logs the access token
-   *
-   * @memberof [AuthService]
-   */
+  
   public async signup(signupDto: SignupDto): Promise<Tokens> {
     const { email, password } = signupDto;
 
@@ -67,24 +48,6 @@ export class AuthService {
     return { accessToken, refreshToken };
   }
 
-  /**
-   * Validates the user's email and password, and if successful, generates access and refresh tokens.
-   *
-   * @async
-   * @param {SigninDto} signinDto - Data transfer object containing the user's email and password for validation.
-   * @returns {Promise<Tokens>} An object containing the accessToken and refreshToken for the validated user.
-   * @throws {BadRequestException} Throws an exception if the email is not found or the password is invalid.
-   *
-   * @example
-   * const signinData = {
-   *   email: "example@example.com",
-   *   password: "Password123+"
-   * };
-   * const tokens = await validateUser(signinData);
-   * console.log(tokens.accessToken);  // Logs the access token
-   *
-   * @memberof [AuthService]
-   */
   public async validateUser(signinDto: SigninDto): Promise<Tokens> {
     this.logger.debug(`Validate user request: ${signinDto.email}`);
     const { email, password } = signinDto;
@@ -114,23 +77,6 @@ export class AuthService {
     return { accessToken, refreshToken };
   }
 
-  /**
-   * Verifies the email of a user based on provided email and hash.
-   *
-   * @async
-   * @param {Object} verifyAccountByEmailPayload - The payload for email verification.
-   * @param {string} verifyAccountByEmailPayload.email - The email address of the user.
-   * @param {string} verifyAccountByEmailPayload.hash - The hash associated with the email verification.
-   *
-   * @returns {Promise<string>} Returns a success message if the verification is successful;
-   * otherwise, returns an 'Invalid action' message.
-   *
-   * @throws Will return 'Invalid action' for any error during the verification process.
-   *
-   * @example
-   * const response = await verifyEmail({ email: 'user@example.com', hash: 'abcd1234' });
-   * console.log(response); // "Success! Account verified" or "Invalid action"
-   */
   async verifyEmail(verifyAccountByEmailPayload: { email: string; hash: string }) {
     try {
       const user: UserDocument = await this.userService.findUserByEmail(verifyAccountByEmailPayload.email);
@@ -145,26 +91,6 @@ export class AuthService {
   }
 
   // Utils
-
-  /**
-   * Generates access and refresh tokens for the given user.
-   *
-   * @async
-   * @private
-   * @param {UserDocument} user - The user document for which the tokens need to be generated.
-   * @returns {Promise<Tokens>} An object containing the generated accessToken and refreshToken.
-   *
-   * @example
-   * const userDocument = {
-   *   id: "123456",
-   *   email: "example@example.com",
-   *   roles: ["admin"]
-   * };
-   * const tokens = await signTokens(userDocument);
-   * console.log(tokens.accessToken);  // Logs the generated access token
-   *
-   * @memberof [AuthService]
-   */
   private async signTokens(user: UserDocument): Promise<Tokens> {
     const [accessToken, refreshToken] = await Promise.all([
       this.jwtService.signAsync(
@@ -178,58 +104,15 @@ export class AuthService {
     ]);
     return { accessToken, refreshToken };
   }
-
-  /**
-   * Hashes the given password using bcrypt.
-   *
-   * @async
-   * @private
-   * @param {string} password - The password to be hashed.
-   * @returns {Promise<string>} The hashed password.
-   *
-   * @example
-   * const hashedPassword = await hashPassword("myPassword123");
-   * console.log(hashedPassword);  // Logs the hashed password
-   *
-   * @memberof [AuthService]
-   */
+  
   private async hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 10);
   }
 
-  /**
-   * Hashes the given token using bcrypt.
-   *
-   * @async
-   * @private
-   * @param {string} token - The token to be hashed.
-   * @returns {Promise<string>} The hashed token.
-   *
-   * @example
-   * const hashedToken = await hashToken("myToken123");
-   * console.log(hashedToken);  // Logs the hashed token
-   *
-   * @memberof [AuthService]
-   */
   private async hashToken(token: string): Promise<string> {
     return await bcrypt.hash(token, 10);
   }
 
-  /**
-   * Compares a plain text string to a hashed string to determine if they match.
-   *
-   * @async
-   * @private
-   * @param {string} plainText - The plain text string to compare.
-   * @param {string} hash - The hashed string to compare against.
-   * @returns {Promise<boolean>} True if the plain text matches the hash, otherwise false.
-   *
-   * @example
-   * const isMatch = await compareHashes("myPassword123", "hashedValue");
-   * console.log(isMatch);  // Logs true or false
-   *
-   * @memberof [AuthService]
-   */
   private async compareHashes(plainText: string, hash: string): Promise<boolean> {
     return await bcrypt.compare(plainText, hash);
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,52 +9,15 @@ export class UsersService {
 
   constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
-  /**
-   * Fetches a user by their user ID from the database.
-   *
-   * @async
-   * @param {string} userId - The ID of the user to retrieve.
-   * @returns {Promise<UserDocument>} Returns a promise that resolves to the user document if found.
-   * @throws {Error} Throws an error if there's an issue with the database operation.
-   */
   public async getMe(userId: string): Promise<UserDocument> {
     return await this.userModel.findById(userId);
   }
 
-  /**
-   * Finds a user by their email address.
-   *
-   * @async
-   * @public
-   * @param {string} email - The email address of the user to be searched for.
-   * @returns {Promise<UserDocument>} The user document if found, otherwise null.
-   *
-   * @example
-   * const user = await usersService.findUserByEmail("example@example.com");
-   * console.log(user);  // Logs the user document if found
-   *
-   * @memberof [UsersService]
-   */
   public async findUserByEmail(email: string): Promise<UserDocument> {
     return await this.userModel.findOne({ email });
   }
 
-  /**
-   * Creates a new user with the provided email and hashed password.
-   * Intended exclusively for the auth service signup function. Should not be used elsewhere.
-   *
-   * @async
-   * @public
-   * @param {string} email - The email address of the new user.
-   * @param {string} hashedPassword - The hashed password for the new user.
-   * @returns {Promise<UserDocument>} The created user document.
-   *
-   * @example
-   * const user = await usersService.createUser("example@example.com", "hashedPassword123");
-   * console.log(user);  // Logs the newly created user document
-   *
-   * @memberof [UsersService]
-   */ public async createUser(email: string, hashedPassword: string): Promise<UserDocument> {
+  public async createUser(email: string, hashedPassword: string): Promise<UserDocument> {
     const newUser = new this.userModel({
       email,
       password: hashedPassword,
